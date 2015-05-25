974543ce202cec6f750d60c67c4b6d5f3282c0f8
KENTO-T <teraoka.kento@tecnos.co.jp>
2015-05-25 14:18:47 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -22,3 +22,26 @@ cacheSolve <- function(x, ...) { #... through x another functions
         x$setinverse(m)
         m
 }
+
+##assignment3
+best <- function(state, outcome) {
+        ## Read outcome data
+        outcome <- read.csv("outcome-of-care-measures.csv",  colClasses = "character")
+        HN <- as.character(outcome[,2])
+        state <- as.numeric(outcome[,7])
+        HA <- as.numeric(outcome[,11])
+        HF <- as.numeric(outcome[,17])
+        Pn <- as.numeric(outcome[,23])
+        outcome2 <- data.frame(HN,state,HA,HF,Pn)
+        names(outcome2)[3]<-"heart attack"
+        names(outcome2)[4]<-"heart failure"
+        names(outcome2)[5]<-"pneumonia"
+        q1<-complete.cases(outcome2)
+        q2<-outcome2[q1,]
+        w1<-split(q2,q2[,2])
+        w2<-w1$"state"
+        w3<-min(w2[,"outcome"])
+        w4<-subset(w2, w2$"outcome"==w3)
+        w5<-as.numeric(w4$"HN")
+        w5
+        }
