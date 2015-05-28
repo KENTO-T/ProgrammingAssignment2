d001316d2c7a5bfb68c4aa0596461d3f73c2b7ff
KENTO-T <teraoka.kento@tecnos.co.jp>
2015-05-28 22:38:24 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -149,3 +149,40 @@ rankall <- function(autcome,num = i) {
         out1<-outcome[,c(2,7,11,17,23)]
         names(out1)[1:5]<-c("hospital","state","heart attack","heart failure","pneumonia")
 }
+
+assignment 2
+## Creates a list of functions that set and get the value of a matrix along with the inverse of that matrix.   Called before cacheSolve().
+## $set is passed the matrix, defines it for future function calls
+## $get returns the matrix
+## $setInverse is passed the inverse and defines it for future function calls
+## $getInverse returns the inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    invMatrix <- NULL
+    set <- function(y) {
+        x <<- y
+        invMatrix <<- NULL
+        
+    }
+    get <- function() x
+    setInverse <- function(inverse) invMatrix <<- inverse
+    getInverse <- function() invMatrix
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+
+## Returns the inverse of the matrix previouisly defined with the makeCacheMatrix() function and prevents duplicate inverse calculations.
+## Pass the list of functions created with makeCacheMatrix(x) where x is the matrix to calculate its inverse.
+
+cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x' where 'x' was previously passed to makeCacheMatrix.
+    invMatrix <- x$getInverse()
+    if(!is.null(invMatrix)) {
+        message("getting cached data")
+        return(invMatrix)
+    }
+    data <- x$get()
+    invMatrix <- solve(data, ...)
+    x$setInverse(invMatrix)
+    invMatrix
+}
